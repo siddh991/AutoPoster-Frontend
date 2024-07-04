9d98cc2d0ff25b1ab9a84f44c1508397284bd5f1
rishabhpatni <rishabh28patni@gmail.com>
2024-07-03 19:32:23 -0700

    undid error

--- src/components/tiktokOauth/Home.js
+++ src/components/tiktokOauth/Home.js
@@ -1,18 +1,14 @@
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
 const Home = () => {
-  // const request_token = async () => {
-  //   const response = await axios.get("https://c4gtukrl53e6zmzyoen4i7pprm0hlsel.lambda-url.us-east-2.on.aws/oauth");
-  //   window.location.href = `${response.data.url}`;
-  // };
-  // const request_token = 'https://www.google.com'
-  const doNothing = () => {
-    // This function does nothing
+  const request_token = async () => {
+    const response = await axios.get("https://c4gtukrl53e6zmzyoen4i7pprm0hlsel.lambda-url.us-east-2.on.aws/oauth");
+    window.location.href = `${response.data.url}`;
   };
 return (
     <div>
       <div>
-        <button onClick={doNothing}>Tik tok</button>
+        <button onClick={request_token}>Tik tok</button>
       </div>
     </div>
   );
